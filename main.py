20f9fa49aa7ec282202ce912fd317daa4715f99b
libbyleonard22 <815989@mystma.org>
2021-09-21 14:34:45 -0500

    Added 5 movies

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 #WRITE YOUR CODE IN THIS FILE
+def topFiveMovies(a, b, c, d, e):
+    return "a", "b", "c", "d", "e"
+
+topFiveMovies("Unbroken", "Finding Dory", "Finding Nemo", "Big Hero 6", "Wall-E")
+
