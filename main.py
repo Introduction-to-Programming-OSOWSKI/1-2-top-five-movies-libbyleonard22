38f902bb6fb500787ac7748405971848404e7e9a
libbyleonard22 <815989@mystma.org>
2021-09-21 14:40:44 -0500

    edit 2

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #WRITE YOUR CODE IN THIS FILE
+
 def topFiveMovies(a, b, c, d, e):
-    return "a", "b", "c", "d", "e"
+    return (a, b, c, d, e)
 
 topFiveMovies("Unbroken", "Finding Dory", "Finding Nemo", "Big Hero 6", "Wall-E")
 
